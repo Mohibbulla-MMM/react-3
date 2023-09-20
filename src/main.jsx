4d70ce55913c5b575f4386a8d68d186003dac5a4
Mohibbulla-mmm <rockychain1020@gmail.com>
2023-09-21 00:18:14 +0600

    all file some edit

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import Router from "./components/Router/Router";
 import "./index.css";
+// import "/apps.css"
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <App />     */}
